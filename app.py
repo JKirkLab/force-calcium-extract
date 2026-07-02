d10cbec664b7a16570e66362077d4d4a93acd81b
mtqiaohub <mtqiao@wustl.edu>
2026-07-02 16:35:18 -0500

    added formatting functionality

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pandas as pd
 from src import convert_voltage as cv
 from src import extract_txt as ex
 from src import select_points as sp
+from src.gen_output import generate_output, generate_force
 
 def set_padded_ylim(ax, time, y, xmin, xmax, pad_frac=0.05):
     mask = (time >= xmin) & (time <= xmax)
@@ -29,77 +30,118 @@ uploaded_files = st.sidebar.file_uploader(
     accept_multiple_files=True
 )
 
+fin_scale = st.number_input(
+    "Force In Scale",
+    value=0.0,
+    step=0.01,
+    format="%.6f"
+)
+
+
+height = st.number_input(
+    "Height",
+    value=0.0,
+    step=0.01,
+    format="%.6f"
+)
+
+width = st.number_input(
+    "Width",
+    value=0.0,
+    step=0.01,
+    format="%.6f"
+)
+
+
+
 rows = []
 
-if uploaded_files:
+
+
+if uploaded_files and height and width:
     st.write("Uploaded files:")
-    for file in uploaded_files:
-        st.subheader(file.name)
+    with st.expander("Show plots"):
+        for file in uploaded_files:
+            st.subheader(file.name)
 
-        df, scale, offset = ex.extract_metadata(file)
+            df, scale, offset = ex.extract_metadata(file)
 
-        df['Voltage'] = df['Force'].apply(
-            lambda x: cv.convert_force_voltage(x, scale, offset)
-        )
-        result = sp.extract_4_points(df["Time_ms"], df['Voltage'], frac=0.015)
+            df['Voltage'] = df['Force'].apply(
+                lambda x: cv.convert_force_voltage(x, scale, offset)
+            )
+            result = sp.extract_4_points(df["Time_ms"], df['Voltage'], frac=0.015)
 
-        time = np.asarray(df["Time_ms"])
+            time = np.asarray(df["Time_ms"])
 
-        force = np.asarray(df["Voltage"])
-        
-        force_smooth = result["smoothed_force"]
+            force = np.asarray(df["Voltage"])
+            
+            force_smooth = result["smoothed_force"]
 
-        p1 = result["p1"]
-        p2 = result["p2"]
-        p3 = result["p3"]
-        p4 = result["p4"]
+            p1 = result["p1"]
+            p2 = result["p2"]
+            p3 = result["p3"]
+            p4 = result["p4"]
 
-        rows.append([file.name,p1['value'], p2['value']])
-        rows.append([file.name,p4['value'], p3['value']])
+            rows.append([file.name + "_contract",p1['value'], p2['value']])
+            rows.append([file.name + "_relax",p4['value'], p3['value']])
 
-        pad = 100  
+            pad = 100  
 
-        xmin1 = min(p1["time_ms"], p2["time_ms"]) - pad
-        xmax1 = max(p1["time_ms"], p2["time_ms"]) + pad
+            xmin1 = min(p1["time_ms"], p2["time_ms"]) - pad
+            xmax1 = max(p1["time_ms"], p2["time_ms"]) + pad
 
-        fig1, ax1 = plt.subplots(figsize=(10, 4))
-        ax1.plot(time, force, alpha=0.3, label="raw")
-        ax1.plot(time, force_smooth, label="smoothed")
-        ax1.scatter(p1["time_ms"], p1["value"], label="p1")
-        ax1.scatter(p2["time_ms"], p2["value"], label="p2")
+            fig1, ax1 = plt.subplots(figsize=(10, 4))
+            ax1.plot(time, force, alpha=0.3, label="raw")
+            ax1.plot(time, force_smooth, label="smoothed")
+            ax1.scatter(p1["time_ms"], p1["value"], label="p1")
+            ax1.scatter(p2["time_ms"], p2["value"], label="p2")
 
-        ax1.set_xlim(xmin1, xmax1)
-        set_padded_ylim(ax1, time, force_smooth, xmin1, xmax1)
+            ax1.set_xlim(xmin1, xmax1)
+            set_padded_ylim(ax1, time, force_smooth, xmin1, xmax1)
 
-        ax1.set_xlabel("Time (ms)")
-        ax1.set_ylabel("Force")
-        ax1.set_title("Zoomed view: p1 and p2")
-        ax1.legend()
-        st.pyplot(fig1)
+            ax1.set_xlabel("Time (ms)")
+            ax1.set_ylabel("Force")
+            ax1.set_title("Zoomed view: p1 and p2")
+            ax1.legend()
+            st.pyplot(fig1)
 
 
-        # Plot 2: p3 & p4
-        xmin2 = min(p3["time_ms"], p4["time_ms"]) - pad
-        xmax2 = max(p3["time_ms"], p4["time_ms"]) + pad
+            # Plot 2: p3 & p4
+            xmin2 = min(p3["time_ms"], p4["time_ms"]) - pad
+            xmax2 = max(p3["time_ms"], p4["time_ms"]) + pad
 
-        fig2, ax2 = plt.subplots(figsize=(10, 4))
-        ax2.plot(time, force, alpha=0.3, label="raw")
-        ax2.plot(time, force_smooth, label="smoothed")
-        ax2.scatter(p3["time_ms"], p3["value"], label="p3")
-        ax2.scatter(p4["time_ms"], p4["value"], label="p4")
+            fig2, ax2 = plt.subplots(figsize=(10, 4))
+            ax2.plot(time, force, alpha=0.3, label="raw")
+            ax2.plot(time, force_smooth, label="smoothed")
+            ax2.scatter(p3["time_ms"], p3["value"], label="p3")
+            ax2.scatter(p4["time_ms"], p4["value"], label="p4")
 
-        ax2.set_xlim(xmin2, xmax2)
-        set_padded_ylim(ax2, time, force_smooth, xmin2, xmax2)
+            ax2.set_xlim(xmin2, xmax2)
+            set_padded_ylim(ax2, time, force_smooth, xmin2, xmax2)
 
-        ax2.set_xlabel("Time (ms)")
-        ax2.set_ylabel("Force")
-        ax2.set_title("Zoomed view: p3 and p4")
-        ax2.legend()
-        st.pyplot(fig2)
-
-        # st.write("p3 Index", p1['index'])
-        # st.write("p3 Index", p3['index'])
+            ax2.set_xlabel("Time (ms)")
+            ax2.set_ylabel("Force")
+            ax2.set_title("Zoomed view: p3 and p4")
+            ax2.legend()
+            st.pyplot(fig2)
 
     if rows:
-        combined_table = pd.DataFrame(rows, columns=["File", "Label", "Value"])
-        st.dataframe(combined_table)
+        # combined_table = pd.DataFrame(rows, columns=["File", "2.1μm", "Slack"])
+        # st.dataframe(combined_table)
+        voltage_df, af_v, cell_nums= generate_output(rows)
+
+        st.write("Height: ", height)
+        st.write("Width: ", width)
+        
+
+        force_df, csa = generate_force(af_v, cell_nums, height, width, fin_scale)
+
+        st.write("CSA: ", csa )
+
+        st.dataframe(voltage_df, hide_index = True, height=38 + 35 * len(voltage_df))
+        st.dataframe(force_df,  hide_index = True, height=38 + 35 * len(force_df))
+
+        
+        
+
+
---
+++ src/gen_output.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+
+def sort_order(row):
+    file_name = row[0]
+    parts = file_name.split("_")
+
+    a = parts[-3]
+    b = parts[-2]
+
+    if a == "100" and b == "1":
+        return (-1,)
+    if a == "100" and b == "2":
+        return (99999,)
+
+    return (int(b),)
+
+def extract_calc(filename):
+    parts = filename.split("_")
+
+    if parts[-1] == "relax":
+        return 0
+    
+    elif parts[-1] == "contract" and parts[-3] == "100":
+        return 100
+    else:
+        return int(parts[-2])
+
+def generate_output(rows):
+    rows.sort(key = sort_order)
+    output = pd.DataFrame(rows, columns = ["filename", "2.1μm", "Slack"])
+    output["cell #"] = output["filename"].apply(extract_calc)
+    
+
+    output["DF (V)"] = output["2.1μm"] - output["Slack"]
+
+    output["AF (V)"] = np.nan
+
+    output.loc[output.index[::2], "AF (V)"] = (
+        output.loc[output.index[::2], "DF (V)"].to_numpy() -
+        output.loc[output.index[1::2], "DF (V)"].to_numpy()
+    )
+    output["cell #"] = output["cell #"].astype(int)
+
+    output["Group"] = np.nan
+
+    mask = output["cell #"] != 0
+
+    af_v = output.loc[mask, "AF (V)"].to_numpy()
+    cell_numbers = output.loc[mask, "cell #"].to_numpy()
+
+
+
+    output.loc[output.index[-1], "Group"] = (af_v[0] - af_v[-1]) / af_v[0]
+    output = output.drop(columns = "filename")
+    return output, af_v, cell_numbers
+
+def generate_force(af_voltage, cell_nums, height, width, fin_scale):
+    csa =((((height + width) / 2) * (10**-3)/2) ** 2) * 3.14159
+    af_voltage = af_voltage[1:]
+    cell_nums = cell_nums[1:]
+    force_df = pd.DataFrame({
+        "Calcium": cell_nums,
+        "Volts": af_voltage,
+    })
+
+    force_df["F (mN)"] = force_df["Volts"] * fin_scale
+    force_df["F (mN/mm2)"] = force_df["F (mN)"] / csa
+    
+    return force_df, csa
